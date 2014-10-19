7840ca119f514d1052c5b15dfc4ae85135235399
cjette <jette@alum.mit.edu>
2014-10-19 12:05:27 -0700

    Scrapped db idea to go with flat files for now

--- ReferenceDatabaseCreator/ReferenceDatabaseCreator/MainPage.xaml.cs
+++ ReferenceDatabaseCreator/ReferenceDatabaseCreator/MainPage.xaml.cs
@@ -8,6 +8,8 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Streams;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -32,12 +34,6 @@ namespace ReferenceDatabaseCreator
 
             this.NavigationCacheMode = NavigationCacheMode.Required;
 
-            _db = new SQLiteConnection("Workouts.db");
-            _db.CreateCommand("PRAGMA foreign_keys = ON").ExecuteNonQuery();
-            _db.DropTable<Workouts>();
-            _db.DropTable<WorkoutPlans>();
-            _db.CreateTable<WorkoutPlans>();
-            _db.CreateTable<Workouts>();
         }
 
         /// <summary>
@@ -56,114 +52,20 @@ namespace ReferenceDatabaseCreator
             // this event is handled for you.
         }
 
-        private void createDatabase_Click(object sender, RoutedEventArgs e)
+        private async void createDatabase_Click(object sender, RoutedEventArgs e)
         {
-            WorkoutPlans workoutPlans = new WorkoutPlans()
-            {
-                Name = "Insanity"
-            };
-
-            _db.Insert(workoutPlans);
-
-            var query = _db.Table<WorkoutPlans>().Where(x => x.Name.Contains("Insanity"));
-            var result = query.ToList()[0];
-
-            List<Workouts> workoutList = new List<Workouts>()
-            {
-                new Workouts()
-                {
-                    Name = "Fit Test",
-                    WorkoutPlanId = result.Id
-                },
-                new Workouts()
-                {
-                    Name = "Plyometric Cardio Circuit",
-                    WorkoutPlanId = result.Id
-                },
-                new Workouts()
-                {
-                    Name = "Cardio Power and Resistance",
-                    WorkoutPlanId = result.Id
-                },
-                new Workouts()
-                {
-                    Name = "Cardio Recovery",
-                    WorkoutPlanId = result.Id
-                },
-                new Workouts()
-                {
-                    Name = "Pure Cardio",
-                    WorkoutPlanId = result.Id
-                },
-                new Workouts()
-                {
-                    Name = "Rest",
-                    WorkoutPlanId = result.Id
-                },
-                new Workouts()
-                {
-                    Name = "Cardio Abs",
-                    WorkoutPlanId = result.Id
-                },
-                new Workouts()
-                {
-                    Name = "Core Cardio and Balance",
-                    WorkoutPlanId = result.Id
-                },
-                new Workouts()
-                {
-                    Name = "MAX Interval Circuit",
-                    WorkoutPlanId = result.Id
-                },
-                new Workouts()
-                {
-                    Name = "MAX Interval Plyometrics",
-                    WorkoutPlanId = result.Id
-                },
-                new Workouts()
-                {
-                    Name = "MAX Cardio Conditioning",
-                    WorkoutPlanId = result.Id
-                },
-                new Workouts()
-                {
-                    Name = "MAX Recovery",
-                    WorkoutPlanId = result.Id
-                },
-                new Workouts()
-                {
-                    Name = "MAX Cardio",
-                    WorkoutPlanId = result.Id
-                }
-            };
-
-            _db.InsertAll(workoutList);
-
-            var query2 = _db.Table<Workouts>().Where(x => x.Name.Contains("MAX"));
-            var result2 = query2.ToList();
-            string name2 = "";
-
+            var folder = Windows.Storage.ApplicationData.Current.LocalFolder;
+            StorageFile file = await folder.CreateFileAsync("WorkoutPlans.txt", CreationCollisionOption.ReplaceExisting);
+            await FileIO.WriteTextAsync(file, "Insanity");
+            
         }
-    }
 
-    [Table("WorkoutPlans")]
-    public class WorkoutPlans
-    {
-        [PrimaryKey, AutoIncrement]
-        public int Id { get; set; }
-
-        public string Name { get; set; }
-    }
-
-    [Table("Workouts")]
-    public class Workouts
-    {
-        [PrimaryKey, AutoIncrement]
-        public int Id { get; set; }
-
-        public string Name { get; set; }
-
-        [ForeignKey(typeof(WorkoutPlans))]
-        public int WorkoutPlanId { get; set; }
+        private async void readFile_Click(object sender, RoutedEventArgs e)
+        {
+            var folder = Windows.Storage.ApplicationData.Current.LocalFolder;
+            StorageFile file = await folder.GetFileAsync("workouts.txt");
+            string result = await FileIO.ReadTextAsync(file);
+            textBlock.Text = result;
+        }
     }
 }
